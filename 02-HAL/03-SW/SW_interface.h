b7c14bd257ec388af15ce9103da16fb3ba4e602b
abderahman adwe <maradonabode@gmail.com>
2023-02-13 01:46:11 +0200

    created the SW_Driver

--- 02-HAL/03-SW/SW_interface.h
+++ 02-HAL/03-SW/SW_interface.h
@@ -18,6 +18,20 @@
 #define SW_EXT_PUll_DOWN 2
 #define SW_FLAUTING      3
  
+#define SW_PIN0 0
+#define SW_PIN1 1
+#define SW_PIN2 2
+#define SW_PIN3 3
+#define SW_PIN4 4
+#define SW_PIN5 5
+#define SW_PIN6 6
+#define SW_PIN7 7
+
+#define SW_PORTA 0
+#define SW_PORTB 1
+#define SW_PORTC 2
+#define SW_PORTD 3
+
 typedef struct 
 {
 	u8 Port       ;
